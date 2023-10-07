e6d7622fdd2932bd22c602c949c107f10f81cca1
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-07 12:24:38 +0530

    dsa

--- index1.js
+++ index1.js
@@ -111,4 +111,26 @@ function powRec(num, power) {
   return num * powRec(num, power - 1);
 }
 
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] == target) return mid;
+    if (arr[mid] < target) {
+      left = mid + 1;
+    }
+    if (arr[mid] > target) {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 4));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 0));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 1));
+
 
