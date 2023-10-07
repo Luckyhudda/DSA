0afb54beb0461b76076d7e2b017333fb90dda562
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-07 12:24:22 +0530

    dsa

--- index1.js
+++ index1.js
@@ -16,63 +16,54 @@ function isPrime(num) {
 // console.log(isPrime(5)); // true
 // console.log(isPrime(1)); // false
 
+function fibonacci(num) {
+  let arr = [];
 
-
-
-function fibonacci(num){
-    let arr = []
-
-    if(num == 0) {
-        return [];
-    } 
-    else if(num ==1){
-        return [0]
-    }else if(num ==2) {
-        return [0,1]
-    }else {
-        arr = [0, 1];
-for (let i = 2; i < num; i++) {
-  arr[i] = arr[i - 1] + arr[i - 2];
-}
+  if (num == 0) {
+    return [];
+  } else if (num == 1) {
+    return [0];
+  } else if (num == 2) {
+    return [0, 1];
+  } else {
+    arr = [0, 1];
+    for (let i = 2; i < num; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2];
     }
-    
- return arr;
+  }
+
+  return arr;
 }
 
 // console.log(fibonacci(7));
 // console.log(fibonacci(1));
 // console.log(fibonacci(0));
 
-
-
-function factorial(num){
-    sum = 1;
-    for (let i = 1; i<=num;i++){
-        sum*= i;
-    }
-    return sum;
+function factorial(num) {
+  sum = 1;
+  for (let i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
 }
 
 // console.log(factorial(5));
 // console.log(factorial(2));
 // console.log(factorial(1));
 
-
-function powTow(num, pw){
-    if(pw == 0) return 1;
-    if(pw == 1) return num;
-    let result = 1;
-    for(let i = 1; i<=pw;i++){
-        result *= num;
-    }
-    return result;
+function powTow(num, pw) {
+  if (pw == 0) return 1;
+  if (pw == 1) return num;
+  let result = 1;
+  for (let i = 1; i <= pw; i++) {
+    result *= num;
+  }
+  return result;
 }
 
 // console.log(powTow(2,2));
 // console.log(powTow(2,4));
 // console.log(powTow(2,1));
-
-
 
 function powThree(num, pw) {
   if (pw == 0) return 1;
@@ -89,35 +80,35 @@ function powThree(num, pw) {
 // console.log(powThree(3,2));
 // console.log(powThree(3,0));
 
-
-function factorialRec(num){
-    if(num <1) return 1;
-    return  num * factorialRec(num-1)
+function factorialRec(num) {
+  if (num < 1) return 1;
+  return num * factorialRec(num - 1);
 }
 // console.log(factorial(4));       // 23
 // console.log(factorial(5));      // 120
 // console.log(factorial(1));     // 1
 
-
 function fiboRecursive(num) {
-  if (num  == 1) {
+  if (num == 1) {
     return 1;
-  } else if(num == 0){
+  } else if (num == 0) {
     return 0;
   }
   return fiboRecursive(num - 1) + fiboRecursive(num - 2);
 }
 
-// console.log(fiboRecursive(5)) 
-// console.log(fiboRecursive(3)) 
+// console.log(fiboRecursive(5))
+// console.log(fiboRecursive(3))
 // console.log(fiboRecursive(7))
 
-function powRec(num, power){
-    if(power == 0){
-        return 1;
-    }
-    if(power == 1){
-        return num;
-    }
-    return num * powRec(num,power -1)
+function powRec(num, power) {
+  if (power == 0) {
+    return 1;
+  }
+  if (power == 1) {
+    return num;
+  }
+  return num * powRec(num, power - 1);
 }
+
+
